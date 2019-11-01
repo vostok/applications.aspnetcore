5f2064d38cfb1608950a3a832819e4b2c36e6c7c
kungurtsev <kungurtsev@skbkontur.ru>
2019-11-01 12:04:43 +0500

    copy code from aspnetcore context

---
+++ Vostok.Hosting.AspNetCore/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Vostok.Context;
+using Vostok.Hosting.AspNetCore.Middlewares;
+
+namespace Vostok.Hosting.AspNetCore
+{
+    /// <summary>
+    /// Extension methods for configuring an application to
+    /// restore distributed <see cref="FlowingContext.Properties"/>
+    /// and <see cref="FlowingContext.Globals"/> from <see cref="HttpContext"/>
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Enables restoring of distributed <see cref="FlowingContext.Properties"/>
+        /// and <see cref="FlowingContext.Globals"/> from <see cref="HttpContext"/>
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder" />.</param>
+        /// <returns></returns>
+        public static void UseDistributedContext(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RestoreDistributedContextMiddleware>();
+        }
+
+        /// <summary>
+        /// Registers an <see cref="RestoreDistributedContextMiddleware" /> service that can restore
+        /// distributed <see cref="FlowingContext.Properties"/>
+        /// and <see cref="FlowingContext.Globals"/> from <see cref="HttpContext"/>
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" />.</param>
+        public static void AddDistributedContext(this IServiceCollection services)
+        {
+            services.AddSingleton<RestoreDistributedContextMiddleware>();
+        }
+    }
+}
