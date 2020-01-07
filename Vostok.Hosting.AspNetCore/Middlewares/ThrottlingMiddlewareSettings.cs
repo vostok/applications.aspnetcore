f99e065ceb6218d69da7f006a9668655e2444581
iloktionov <iloktionov@skbkontur.ru>
2020-01-07 17:36:46 +0500

    Basic implementation of ThrottlingMiddleware.

---
+++ Vostok.Hosting.AspNetCore/Middlewares/ThrottlingMiddlewareSettings.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Hosting.AspNetCore.Middlewares
+{
+    [PublicAPI]
+    public class ThrottlingMiddlewareSettings
+    {
+        public int RejectionResponseCode { get; set; } = 429;
+    }
+}
