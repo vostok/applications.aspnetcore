fa537cd9b2a112968c397a7e79501d6b2b93e83c
iloktionov <iloktionov@skbkontur.ru>
2020-01-07 17:57:33 +0500

    Removed useless xml-docs from methods of IVostokAspNetCoreApplicationBuilder.

--- Vostok.Hosting.AspNetCore/IVostokAspNetCoreApplicationBuilder.cs
+++ Vostok.Hosting.AspNetCore/IVostokAspNetCoreApplicationBuilder.cs
@@ -8,52 +8,27 @@ namespace Vostok.Hosting.AspNetCore
 {
     /// <summary>
     /// <para>Represents a configuration of <see cref="VostokAspNetCoreApplication{TStartup}"/> builder which must be filled during <see cref="VostokAspNetCoreApplication{TStartup}.Setup"/>.</para>
-    /// <para>You can configure <see cref="IWebHostBuilder"/> and customize built-in Vostok middlewares.</para>
+    /// <para>Allows to configure <see cref="IWebHostBuilder"/> and customize built-in Vostok middlewares.</para>
     /// </summary>
     [PublicAPI]
     public interface IVostokAspNetCoreApplicationBuilder
     {
-        /// <summary>
-        /// Delegate which configures <see cref="IWebHostBuilder"/>.
-        /// </summary>
         IVostokAspNetCoreApplicationBuilder SetupWebHost([NotNull] Action<IWebHostBuilder> setup);
 
-        /// <summary>
-        /// Delegate which configures <see cref="LoggingMiddlewareSettings"/>.
-        /// </summary>
         IVostokAspNetCoreApplicationBuilder SetupLogging([NotNull] Action<LoggingMiddlewareSettings> setup);
 
-        /// <summary>
-        /// Delegate which configures <see cref="TracingMiddlewareSettings"/>.
-        /// </summary>
         IVostokAspNetCoreApplicationBuilder SetupTracing([NotNull] Action<TracingMiddlewareSettings> setup);
 
-        /// <summary>
-        /// <para>Denies request processing, if local datacenter is not active.</para>
-        /// <para>Use this option only if your application hosted in multiple datacenters.</para>
-        /// </summary>
+        IVostokAspNetCoreApplicationBuilder SetupThrottling([NotNull] Action<ThrottlingMiddlewareSettings> setup);
+
         IVostokAspNetCoreApplicationBuilder DenyRequestsIfNotInActiveDatacenter(int denyResponseCode = (int)Clusterclient.Core.Model.ResponseCode.ServiceUnavailable);
 
-        /// <summary>
-        /// Delegate which configures <see cref="PingApiMiddlewareSettings"/>.
-        /// </summary>
         IVostokAspNetCoreApplicationBuilder SetupPingApi([NotNull] Action<PingApiMiddlewareSettings> setup);
 
-        /// <summary>
-        /// Delegate which configures <see cref="FillRequestInfoMiddlewareSettings"/>.
-        /// </summary>
         IVostokAspNetCoreApplicationBuilder SetupFillRequestInfo([NotNull] Action<FillRequestInfoMiddlewareSettings> setup);
 
-        /// <summary>
-        /// Delegate which configures <see cref="RestoreDistributedContextMiddlewareSettings"/>.
-        /// </summary>
         IVostokAspNetCoreApplicationBuilder SetupRestoreDistributedContext([NotNull] Action<RestoreDistributedContextMiddlewareSettings> setup);
-
-        /// <summary>
-        /// Delegate which configures <see cref="VostokLoggerProviderSettings"/>.
-        /// </summary>
+        
         IVostokAspNetCoreApplicationBuilder SetupMicrosoftLog([NotNull] Action<VostokLoggerProviderSettings> setup);
-
-        IVostokAspNetCoreApplicationBuilder SetupThrottling([NotNull] Action<ThrottlingMiddlewareSettings> setup);
     }
 }
