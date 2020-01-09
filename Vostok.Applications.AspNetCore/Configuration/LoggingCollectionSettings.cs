6dd8d6da2d7a3afbf2a4a65bb60e9a9b1df99351
iloktionov <iloktionov@skbkontur.ru>
2020-01-09 20:10:37 +0500

    LoggingCollectionSettings: added a ctor with flat boolean value.

--- Vostok.Applications.AspNetCore/Configuration/LoggingCollectionSettings.cs
+++ Vostok.Applications.AspNetCore/Configuration/LoggingCollectionSettings.cs
@@ -14,8 +14,13 @@ namespace Vostok.Applications.AspNetCore.Configuration
         public LoggingCollectionSettings([NotNull] Func<HttpRequest, bool> enabled) =>
             Enabled = enabled ?? throw new ArgumentNullException(nameof(enabled));
 
+        public LoggingCollectionSettings(bool enabled)
+            : this(_ => enabled)
+        {
+        }
+
         /// <summary>
-        /// A delegate that decides whether or not to log parameters of given <see cref="HttpRequest"/>.
+        /// A delegate that decides whether or not to log parameters of given <see cref="HttpRequest"/> or <see cref="HttpResponse"/>.
         /// </summary>
         [NotNull]
         public Func<HttpRequest, bool> Enabled { get; }
@@ -23,19 +28,19 @@ namespace Vostok.Applications.AspNetCore.Configuration
         /// <summary>
         /// <para>Whitelist of parameter keys to be logged.</para>
         /// <para><c>null</c> value allows all keys.</para>
+        /// <para>Takes precedence over <see cref="BlacklistKeys"/>.</para>
         /// </summary>
         [CanBeNull]
         public IReadOnlyCollection<string> WhitelistKeys { get; set; }
 
         /// <summary>
         /// <para>Blacklist of parameter keys to be logged.</para>
-        /// <para>Will be applied, if <see cref="WhitelistKeys"/> is <c>null</c>.</para>
         /// <para><c>null</c> value allows all keys.</para>
         /// </summary>
         [CanBeNull]
         public IReadOnlyCollection<string> BlacklistKeys { get; set; }
 
         public static implicit operator LoggingCollectionSettings(bool enabled) =>
-            new LoggingCollectionSettings(_ => enabled);
+            new LoggingCollectionSettings(enabled);
     }
 }
