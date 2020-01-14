13e27e47586551762b2232e6a666e91325688d61
iloktionov <iloktionov@skbkontur.ru>
2020-01-14 17:45:26 +0500

    Xml-docs for IVostokThrottlingBuilder.

--- Vostok.Applications.AspNetCore/Builders/IVostokThrottlingBuilder.cs
+++ Vostok.Applications.AspNetCore/Builders/IVostokThrottlingBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using JetBrains.Annotations;
 using Vostok.Applications.AspNetCore.Configuration;
+using Vostok.Throttling;
 using Vostok.Throttling.Config;
 using Vostok.Throttling.Quotas;
 
@@ -9,18 +10,43 @@ namespace Vostok.Applications.AspNetCore.Builders
     [PublicAPI]
     public interface IVostokThrottlingBuilder
     {
+        /// <summary>
+        /// <para>Configures throttling essentials, such as capacity and queue limits, to be obtained from given <paramref name="essentialsProvider"/>.</para>
+        /// <para>See <see cref="ThrottlingEssentials"/> for more info.</para>
+        /// </summary>
         [NotNull]
         IVostokThrottlingBuilder UseEssentials([NotNull] Func<ThrottlingEssentials> essentialsProvider);
 
-        // CR(kungurtsev): не понятно, что за проперти такие, в рамках обработки запроса.
-        // CR(kungurtsev): тут явно не хватает доки.
-        // CR(kungurtsev): не сразу понимаешь, что есть проперти, которые (name-value), и есть квоты к ним.
+        /// <summary>
+        /// <para>Adds a quota for the property with given <paramref name="propertyName"/> and options fetched from <paramref name="quotaOptionsProvider"/>.</para>
+        /// <para>Property quotas allow to explicitly limit parallelism of requests with some definite property values.</para>
+        /// <para>By default, internal middleware adds all <see cref="WellKnownThrottlingProperties"/> except for the url:</para>
+        /// <list type="bullet">
+        ///     <item><description><see cref="WellKnownThrottlingProperties.Consumer"/></description></item>
+        ///     <item><description><see cref="WellKnownThrottlingProperties.Priority"/></description></item>
+        ///     <item><description><see cref="WellKnownThrottlingProperties.Method"/></description></item>
+        /// </list>
+        /// <para>When dealing with these well-known properties, it is recommended to use extensions over this method instead:</para>
+        /// <list type="bullet">
+        ///     <item><description><see cref="IVostokThrottlingBuilderExtensions.UseConsumerQuota"/></description></item>
+        ///     <item><description><see cref="IVostokThrottlingBuilderExtensions.UsePriorityQuota"/></description></item>
+        ///     <item><description><see cref="IVostokThrottlingBuilderExtensions.UseMethodQuota"/></description></item>
+        ///     <item><description><see cref="IVostokThrottlingBuilderExtensions.UseUrlQuota"/></description></item>
+        /// </list>
+        /// </summary>
         [NotNull]
         IVostokThrottlingBuilder UsePropertyQuota([NotNull] string propertyName, [NotNull] Func<PropertyQuotaOptions> quotaOptionsProvider);
 
+        /// <summary>
+        /// <para>Adds an instance of custom user-implemented quota.</para>
+        /// <para>See <see cref="IThrottlingQuota"/> for more info.</para>
+        /// </summary>
         [NotNull]
         IVostokThrottlingBuilder UseCustomQuota([NotNull] IThrottlingQuota quota);
 
+        /// <summary>
+        /// Allows to customize advanced settings pertinent to internal middleware responsible for request throttling.
+        /// </summary>
         [NotNull]
         IVostokThrottlingBuilder CustomizeSettings([NotNull] Action<ThrottlingSettings> customization);
     }
--- Vostok.Applications.AspNetCore/Builders/IVostokThrottlingBuilderExtensions.cs
+++ Vostok.Applications.AspNetCore/Builders/IVostokThrottlingBuilderExtensions.cs
@@ -8,32 +8,54 @@ namespace Vostok.Applications.AspNetCore.Builders
     [PublicAPI]
     public static class IVostokThrottlingBuilderExtensions
     {
+        /// <summary>
+        /// Disables the throttling middleware altogether.
+        /// </summary>
         [NotNull]
         public static IVostokThrottlingBuilder DisableThrottling([NotNull] this IVostokThrottlingBuilder builder)
             => builder.CustomizeSettings(s => s.Enabled = _ => false);
 
+        /// <summary>
+        /// Disables generation of throttling metrics.
+        /// </summary>
         [NotNull]
         public static IVostokThrottlingBuilder DisableMetrics([NotNull] this IVostokThrottlingBuilder builder)
             => builder.CustomizeSettings(s => s.Metrics = null);
 
+        /// <summary>
+        /// <para>Sets up a quota on the <see cref="WellKnownThrottlingProperties.Consumer"/> request property configured by given <paramref name="quotaOptionsProvider"/>.</para>
+        /// <para>See <see cref="IVostokThrottlingBuilder.UsePropertyQuota"/> for additional info on property quotas.</para>
+        /// </summary>
         [NotNull]
         public static IVostokThrottlingBuilder UseConsumerQuota([NotNull] this IVostokThrottlingBuilder builder, [NotNull] Func<PropertyQuotaOptions> quotaOptionsProvider) =>
             builder
                 .CustomizeSettings(settings => settings.AddConsumerProperty = true)
                 .UsePropertyQuota(WellKnownThrottlingProperties.Consumer, quotaOptionsProvider);
 
+        /// <summary>
+        /// <para>Sets up a quota on the <see cref="WellKnownThrottlingProperties.Priority"/> request property configured by given <paramref name="quotaOptionsProvider"/>.</para>
+        /// <para>See <see cref="IVostokThrottlingBuilder.UsePropertyQuota"/> for additional info on property quotas.</para>
+        /// </summary>
         [NotNull]
         public static IVostokThrottlingBuilder UsePriorityQuota([NotNull] this IVostokThrottlingBuilder builder, [NotNull] Func<PropertyQuotaOptions> quotaOptionsProvider) => 
             builder
                 .CustomizeSettings(settings => settings.AddPriorityProperty = true)
                 .UsePropertyQuota(WellKnownThrottlingProperties.Priority, quotaOptionsProvider);
 
+        /// <summary>
+        /// <para>Sets up a quota on the <see cref="WellKnownThrottlingProperties.Method"/> request property configured by given <paramref name="quotaOptionsProvider"/>.</para>
+        /// <para>See <see cref="IVostokThrottlingBuilder.UsePropertyQuota"/> for additional info on property quotas.</para>
+        /// </summary>
         [NotNull]
         public static IVostokThrottlingBuilder UseMethodQuota([NotNull] this IVostokThrottlingBuilder builder, [NotNull] Func<PropertyQuotaOptions> quotaOptionsProvider) => 
             builder
                 .CustomizeSettings(settings => settings.AddMethodProperty = true)
                 .UsePropertyQuota(WellKnownThrottlingProperties.Method, quotaOptionsProvider);
 
+        /// <summary>
+        /// <para>Sets up a quota on the <see cref="WellKnownThrottlingProperties.Url"/> request property configured by given <paramref name="quotaOptionsProvider"/>.</para>
+        /// <para>See <see cref="IVostokThrottlingBuilder.UsePropertyQuota"/> for additional info on property quotas.</para>
+        /// </summary>
         [NotNull]
         public static IVostokThrottlingBuilder UseUrlQuota([NotNull] this IVostokThrottlingBuilder builder, [NotNull] Func<PropertyQuotaOptions> quotaOptionsProvider) => 
             builder
