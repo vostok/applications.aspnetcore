6ff1877dbf9224cc0c791eb646387a560e3f2091
iloktionov <iloktionov@skbkontur.ru>
2020-03-07 17:56:37 +0500

    Refactoring: extracted VostokWebHostBuilder.

--- Vostok.Applications.AspNetCore/Builders/VostokAspNetCoreApplicationBuilder.cs
+++ Vostok.Applications.AspNetCore/Builders/VostokAspNetCoreApplicationBuilder.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Vostok.Applications.AspNetCore.Configuration;
 using Vostok.Applications.AspNetCore.Helpers;
-using Vostok.Applications.AspNetCore.Models;
-using Vostok.Applications.AspNetCore.StartupFilters;
-using Vostok.Commons.Helpers;
 using Vostok.Commons.Threading;
-using Vostok.Commons.Time;
 using Vostok.Context;
 using Vostok.Hosting.Abstractions;
 using Vostok.Logging.Microsoft;
-using Vostok.ServiceDiscovery.Abstractions;
 
 namespace Vostok.Applications.AspNetCore.Builders
 {
@@ -21,19 +15,15 @@ namespace Vostok.Applications.AspNetCore.Builders
         where TStartup : class
     {
         private readonly IVostokHostingEnvironment environment;
-        private readonly AtomicBoolean webHostEnabled;
         private readonly VostokKestrelBuilder kestrelBuilder;
+        private readonly VostokThrottlingBuilder throttlingBuilder;
         private readonly VostokMiddlewaresBuilder middlewaresBuilder;
         private readonly VostokNetCoreApplicationBuilder baseHostBuilder;
-        private readonly Customization<IWebHostBuilder> webHostBuilderCustomization;
-
-        private readonly VostokThrottlingBuilder throttlingBuilder;
+        private readonly VostokWebHostBuilder<TStartup> webHostBuilder;
 
         public VostokAspNetCoreApplicationBuilder(IVostokHostingEnvironment environment, List<IDisposable> disposables, AtomicBoolean initialized)
         {
             this.environment = environment;
-
-            webHostEnabled = true;
 
             baseHostBuilder = new VostokNetCoreApplicationBuilder(environment);
             baseHostBuilder.SetupMicrosoftLog(
@@ -47,65 +37,19 @@ namespace Vostok.Applications.AspNetCore.Builders
             kestrelBuilder = new VostokKestrelBuilder();
             throttlingBuilder = new VostokThrottlingBuilder(environment, disposables);
             middlewaresBuilder = new VostokMiddlewaresBuilder(throttlingBuilder, initialized);
-
-            webHostBuilderCustomization = new Customization<IWebHostBuilder>();
+            webHostBuilder = new VostokWebHostBuilder<TStartup>(environment, kestrelBuilder, middlewaresBuilder);
         }
 
         public IHost Build()
         {
             using (FlowingContext.Globals.Use(environment))
             {
-                var hostBuilder = baseHostBuilder.CreateHostBuilder();
+                var genericHostBuilder = baseHostBuilder.CreateHostBuilder();
 
-                if (webHostEnabled)
-                {
-                    hostBuilder.ConfigureServices(middlewaresBuilder.Register);
+                webHostBuilder.ConfigureWebHost(genericHostBuilder);
 
-                    hostBuilder.ConfigureWebHostDefaults(
-                        webHostBuilder =>
-                        {
-                            ConfigureUrl(webHostBuilder, environment);
-
-                            var urlsBefore = webHostBuilder.GetSetting(WebHostDefaults.ServerUrlsKey);
-
-                            webHostBuilder.UseKestrel(kestrelBuilder.ConfigureKestrel);
-                            webHostBuilder.UseSockets(kestrelBuilder.ConfigureSocketTransport);
-                            webHostBuilder.UseShutdownTimeout(environment.ShutdownTimeout.Cut(100.Milliseconds(), 0.05));
-
-                            if (typeof(TStartup) != typeof(EmptyStartup))
-                                webHostBuilder.UseStartup<TStartup>();
-
-                            webHostBuilderCustomization.Customize(webHostBuilder);
-
-                            var urlsAfter = webHostBuilder.GetSetting(WebHostDefaults.ServerUrlsKey);
-                            EnsureUrlsNotChanged(urlsBefore, urlsAfter);
-                        });
-                }
-
-                return hostBuilder.Build();
+                return genericHostBuilder.Build();
             }
-        }
-
-        private static void ConfigureUrl(IWebHostBuilder builder, IVostokHostingEnvironment environment)
-        {
-            if (!environment.ServiceBeacon.ReplicaInfo.TryGetUrl(out var url))
-                throw new Exception("Port or url should be configured in ServiceBeacon using VostokHostingEnvironmentSetup.");
-
-            builder = builder.UseUrls($"{url.Scheme}://*:{url.Port}/");
-
-            builder.ConfigureServices(services => services.AddTransient<IStartupFilter>(_ => new UrlPathStartupFilter(environment)));
-        }
-
-        private void EnsureUrlsNotChanged(string urlsBefore, string urlsAfter)
-        {
-            if (urlsAfter.Contains(urlsBefore))
-                return;
-
-            throw new Exception(
-                "Application url should be configured in ServiceBeacon instead of WebHostBuilder.\n" +
-                $"ServiceBeacon url: '{urlsBefore}'. WebHostBuilder urls: '{urlsAfter}'.\n" +
-                "To configure application port (without url) use VostokHostingEnvironmentSetup extension: `vostokHostingEnvironmentSetup.SetPort(...)`.\n" +
-                "To configure application url use VostokHostingEnvironmentSetup: `vostokHostingEnvironmentSetup.SetupServiceBeacon(serviceBeaconBuilder => serviceBeaconBuilder.SetupReplicaInfo(replicaInfo => replicaInfo.SetUrl(...)))`.");
         }
 
         #region SetupComponents
@@ -118,13 +62,13 @@ namespace Vostok.Applications.AspNetCore.Builders
 
         public IVostokAspNetCoreApplicationBuilder DisableWebHost()
         {
-            webHostEnabled.Value = false;
+            webHostBuilder.Disable();
             return this;
         }
 
         public IVostokAspNetCoreApplicationBuilder SetupWebHost(Action<IWebHostBuilder> setup)
         {
-            webHostBuilderCustomization.AddCustomization(setup ?? throw new ArgumentNullException(nameof(setup)));
+            webHostBuilder.Customize(setup ?? throw new ArgumentNullException(nameof(setup)));
             return this;
         }
 
---
+++ Vostok.Applications.AspNetCore/Builders/VostokWebHostBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vostok.Applications.AspNetCore.Models;
+using Vostok.Applications.AspNetCore.StartupFilters;
+using Vostok.Commons.Helpers;
+using Vostok.Commons.Threading;
+using Vostok.Commons.Time;
+using Vostok.Hosting.Abstractions;
+using Vostok.ServiceDiscovery.Abstractions;
+
+namespace Vostok.Applications.AspNetCore.Builders
+{
+    internal class VostokWebHostBuilder<TStartup>
+        where TStartup : class
+    {
+        private readonly IVostokHostingEnvironment environment;
+        private readonly VostokKestrelBuilder kestrelBuilder;
+        private readonly VostokMiddlewaresBuilder middlewaresBuilder;
+
+        private readonly AtomicBoolean webHostEnabled = true;
+        private readonly Customization<IWebHostBuilder> webHostCustomization = new Customization<IWebHostBuilder>();
+
+        public VostokWebHostBuilder(
+            IVostokHostingEnvironment environment, 
+            VostokKestrelBuilder kestrelBuilder, 
+            VostokMiddlewaresBuilder middlewaresBuilder)
+        {
+            this.environment = environment;
+            this.kestrelBuilder = kestrelBuilder;
+            this.middlewaresBuilder = middlewaresBuilder;
+        }
+
+        public void Disable()
+            => webHostEnabled.Value = false;
+
+        public void Customize(Action<IWebHostBuilder> customization)
+            => webHostCustomization.AddCustomization(customization);
+
+        public void ConfigureWebHost(IHostBuilder genericHostBuilder)
+        {
+            if (webHostEnabled)
+            {
+                genericHostBuilder.ConfigureServices(middlewaresBuilder.Register);
+
+                genericHostBuilder.ConfigureWebHostDefaults(ConfigureWebHost);
+            }
+        }
+
+        private void ConfigureWebHost(IWebHostBuilder webHostBuilder)
+        {
+            ConfigureUrl(webHostBuilder);
+
+            var urlsBefore = webHostBuilder.GetSetting(WebHostDefaults.ServerUrlsKey);
+
+            webHostBuilder.UseKestrel(kestrelBuilder.ConfigureKestrel);
+            webHostBuilder.UseSockets(kestrelBuilder.ConfigureSocketTransport);
+            webHostBuilder.UseShutdownTimeout(environment.ShutdownTimeout.Cut(100.Milliseconds(), 0.05));
+
+            if (typeof(TStartup) != typeof(EmptyStartup))
+                webHostBuilder.UseStartup<TStartup>();
+
+            webHostCustomization.Customize(webHostBuilder);
+
+            EnsureUrlsNotChanged(urlsBefore, webHostBuilder.GetSetting(WebHostDefaults.ServerUrlsKey));
+        }
+
+        private void ConfigureUrl(IWebHostBuilder webHostBuilder)
+        {
+            if (!environment.ServiceBeacon.ReplicaInfo.TryGetUrl(out var url))
+                throw new Exception("Port or url should be configured in ServiceBeacon using VostokHostingEnvironmentSetup.");
+
+            webHostBuilder = webHostBuilder.UseUrls($"{url.Scheme}://*:{url.Port}/");
+
+            webHostBuilder.ConfigureServices(services => services.AddTransient<IStartupFilter>(_ => new UrlPathStartupFilter(environment)));
+        }
+
+        private static void EnsureUrlsNotChanged(string urlsBefore, string urlsAfter)
+        {
+            if (urlsAfter.Contains(urlsBefore))
+                return;
+
+            throw new Exception(
+                "Application url should be configured in ServiceBeacon instead of WebHostBuilder.\n" +
+                $"ServiceBeacon url: '{urlsBefore}'. WebHostBuilder urls: '{urlsAfter}'.\n" +
+                "To configure application port (without url) use VostokHostingEnvironmentSetup extension: `vostokHostingEnvironmentSetup.SetPort(...)`.\n" +
+                "To configure application url use VostokHostingEnvironmentSetup: `vostokHostingEnvironmentSetup.SetupServiceBeacon(serviceBeaconBuilder => serviceBeaconBuilder.SetupReplicaInfo(replicaInfo => replicaInfo.SetUrl(...)))`.");
+        }
+    }
+}
