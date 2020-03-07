2803630581ed02ce18cef7dfe47188532f85870f
iloktionov <iloktionov@skbkontur.ru>
2020-03-07 20:56:29 +0500

    + WebHostFactory, WebHostManager.

---
+++ Vostok.Applications.AspNetCore/Helpers/WebHostFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Vostok.Commons.Helpers;
+using Vostok.Hosting.Abstractions;
+using Vostok.Logging.Microsoft;
+
+namespace Vostok.Applications.AspNetCore.Helpers
+{
+    internal class WebHostFactory
+    {
+        private readonly IVostokHostingEnvironment environment;
+
+        private readonly Customization<VostokLoggerProviderSettings> loggerCustomization = new Customization<VostokLoggerProviderSettings>();
+
+        public WebHostFactory(IVostokHostingEnvironment environment)
+            => this.environment = environment;
+
+        public IWebHostBuilder CreateHostBuilder()
+        {
+            var hostBuilder = WebHost.CreateDefaultBuilder();
+
+            hostBuilder.ConfigureLogging(log => log.AddVostokLogging(environment, GetLoggerSettings()));
+
+            hostBuilder.ConfigureAppConfiguration(config => config.AddVostokSources(environment));
+
+            hostBuilder.ConfigureServices(services => services.AddVostokEnvironment(environment));
+
+            return hostBuilder;
+        }
+
+        public void SetupLogger(Action<VostokLoggerProviderSettings> setup)
+            => loggerCustomization.AddCustomization(setup ?? throw new ArgumentNullException(nameof(setup)));
+
+        private VostokLoggerProviderSettings GetLoggerSettings()
+            => loggerCustomization.Customize(new VostokLoggerProviderSettings());
+    }
+}
---
+++ Vostok.Applications.AspNetCore/Helpers/WebHostManager.cs
@@ -0,0 +1,72 @@
+﻿#pragma warning disable 618
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Vostok.Commons.Helpers.Extensions;
+using Vostok.Logging.Abstractions;
+using IApplicationLifetime = Microsoft.AspNetCore.Hosting.IApplicationLifetime;
+
+// ReSharper disable MethodSupportsCancellation
+
+namespace Vostok.Applications.AspNetCore.Helpers
+{
+    internal class WebHostManager
+    {
+        private readonly IWebHost webHost;
+        private readonly ILog log;
+        private volatile IApplicationLifetime lifetime;
+        private volatile IDisposable shutdownRegistration;
+
+        public WebHostManager(IWebHost webHost, ILog log)
+        {
+            this.webHost = webHost;
+            this.log = log;
+        }
+
+        public IServiceProvider Services => webHost.Services;
+
+        public async Task StartHostAsync(CancellationToken shutdownToken)
+        {
+            lifetime = (IApplicationLifetime)Services.GetService(typeof(IApplicationLifetime));
+
+            var environment = (IHostEnvironment)Services.GetService(typeof(IHostEnvironment));
+
+            shutdownRegistration = shutdownToken.Register(
+                () => webHost
+                    .StopAsync()
+                    .ContinueWith(t => log.Error(t.Exception, "Failed to stop WebHost."), TaskContinuationOptions.OnlyOnFaulted));
+
+            log.Info("Starting WebHost.");
+
+            log.Info("Hosting environment: {HostingEnvironment}.", environment.EnvironmentName);
+
+            await webHost.StartAsync(shutdownToken);
+
+            await lifetime.ApplicationStarted.WaitAsync();
+
+            log.Info("WebHost started.");
+        }
+
+        public async Task RunHostAsync()
+        {
+            await lifetime.ApplicationStopping.WaitAsync();
+
+            log.Info("Stopping WebHost.");
+
+            await lifetime.ApplicationStopped.WaitAsync();
+
+            log.Info("WebHost stopped.");
+
+            webHost.Dispose();
+        }
+
+        public void Dispose()
+        {
+            webHost?.Dispose();
+            shutdownRegistration?.Dispose();
+        }
+    }
+}
