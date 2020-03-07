7e338df2e01c8bbc8056e93b6c64646ef172aa4e
iloktionov <iloktionov@skbkontur.ru>
2020-03-07 20:22:13 +0500

    Refactoring: AddVostokEnvironment extension for IServiceCollection.

--- Vostok.Applications.AspNetCore/Builders/VostokNetCoreApplicationBuilder.cs
+++ Vostok.Applications.AspNetCore/Builders/VostokNetCoreApplicationBuilder.cs
@@ -63,37 +63,10 @@ namespace Vostok.Applications.AspNetCore.Builders
             return this;
         }
 
-        private static void RegisterTypes(IHostBuilder builder, IVostokHostingEnvironment environment) =>
-            builder.ConfigureServices(
-                services =>
-                {
-                    services
-                        .AddSingleton(environment)
-                        .AddSingleton(environment.ApplicationIdentity)
-                        .AddSingleton(environment.ApplicationLimits)
-                        .AddTransient(_ => environment.ApplicationReplicationInfo)
-                        .AddSingleton(environment.Metrics)
-                        .AddSingleton(environment.Log)
-                        .AddSingleton(environment.Tracer)
-                        .AddSingleton(environment.HerculesSink)
-                        .AddSingleton(environment.ConfigurationSource)
-                        .AddSingleton(environment.ConfigurationProvider)
-                        .AddSingleton(environment.ClusterConfigClient)
-                        .AddSingleton(environment.ServiceBeacon)
-                        .AddSingleton(environment.ServiceLocator)
-                        .AddSingleton(environment.ContextGlobals)
-                        .AddSingleton(environment.ContextProperties)
-                        .AddSingleton(environment.ContextConfiguration)
-                        .AddSingleton(environment.Datacenters)
-                        .AddSingleton(environment.HostExtensions);
+        private static void RegisterTypes(IHostBuilder builder, IVostokHostingEnvironment environment) 
+            => builder.ConfigureServices(services => services.AddVostokEnvironment(environment));
 
-                    foreach (var (type, obj) in environment.HostExtensions.GetAll())
-                    {
-                        services.AddSingleton(type, obj);
-                    }
-                });
-
-        private ILoggerProvider CreateMicrosoftLog() =>
-            new VostokLoggerProvider(environment.Log, microsoftLogCustomization.Customize(new VostokLoggerProviderSettings()));
+        private ILoggerProvider CreateMicrosoftLog() 
+            => new VostokLoggerProvider(environment.Log, microsoftLogCustomization.Customize(new VostokLoggerProviderSettings()));
     }
 }
