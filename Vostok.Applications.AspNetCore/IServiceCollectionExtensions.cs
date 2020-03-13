c34cfb96b5fbb12b0cb2643da27bf1edc755e215
iloktionov <iloktionov@skbkontur.ru>
2020-03-13 20:09:21 +0500

    IServiceCollectionExtensions: removed useless parameterless extensions.

--- Vostok.Applications.AspNetCore/IServiceCollectionExtensions.cs
+++ Vostok.Applications.AspNetCore/IServiceCollectionExtensions.cs
@@ -12,20 +12,8 @@ namespace Vostok.Applications.AspNetCore
         /// <summary>
         /// Adds <see cref="DistributedContextSettings"/> to the specified service collection.
         /// </summary>
-        public static IServiceCollection AddVostokDistributedContext(this IServiceCollection services)
-            => services.AddVostokDistributedContext(_ => {});
-
-        /// <summary>
-        /// Adds <see cref="DistributedContextSettings"/> to the specified service collection.
-        /// </summary>
         public static IServiceCollection AddVostokDistributedContext(this IServiceCollection services, Action<DistributedContextSettings> setup)
             => services.Configure(setup);
-
-        /// <summary>
-        /// Adds <see cref="DatacenterAwarenessSettings"/> to the specified service collection.
-        /// </summary>
-        public static IServiceCollection AddVostokDatacenterAwareness(this IServiceCollection services)
-            => services.AddVostokDatacenterAwareness(_ => {});
 
         /// <summary>
         /// Adds <see cref="DatacenterAwarenessSettings"/> to the specified service collection.
@@ -36,20 +24,8 @@ namespace Vostok.Applications.AspNetCore
         /// <summary>
         /// Adds <see cref="FillRequestInfoSettings"/> to the specified service collection.
         /// </summary>
-        public static IServiceCollection AddVostokRequestInfo(this IServiceCollection services)
-            => services.AddVostokRequestInfo(_ => { });
-
-        /// <summary>
-        /// Adds <see cref="FillRequestInfoSettings"/> to the specified service collection.
-        /// </summary>
         public static IServiceCollection AddVostokRequestInfo(this IServiceCollection services, Action<FillRequestInfoSettings> setup)
             => services.Configure(setup);
-
-        /// <summary>
-        /// Adds <see cref="TracingSettings"/> to the specified service collection.
-        /// </summary>
-        public static IServiceCollection AddVostokTracing(this IServiceCollection services)
-            => services.AddVostokTracing(_ => {});
 
         /// <summary>
         /// Adds <see cref="TracingSettings"/> to the specified service collection.
@@ -74,20 +50,8 @@ namespace Vostok.Applications.AspNetCore
         /// <summary>
         /// Adds <see cref="PingApiSettings"/> to the specified service collection.
         /// </summary>
-        public static IServiceCollection AddVostokPingApi(this IServiceCollection services)
-            => services.AddVostokPingApi(_ => {});
-
-        /// <summary>
-        /// Adds <see cref="PingApiSettings"/> to the specified service collection.
-        /// </summary>
         public static IServiceCollection AddVostokPingApi(this IServiceCollection services, Action<PingApiSettings> setup)
             => services.Configure(setup);
-
-        /// <summary>
-        /// Adds <see cref="LoggingSettings"/> to the specified service collection.
-        /// </summary>
-        public static IServiceCollection AddVostokRequestLogging(this IServiceCollection services)
-            => services.AddVostokRequestLogging(_ => {});
 
         /// <summary>
         /// Adds <see cref="LoggingSettings"/> to the specified service collection.
