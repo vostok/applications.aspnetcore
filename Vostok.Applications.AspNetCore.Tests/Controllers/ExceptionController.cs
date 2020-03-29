709cab7a10966e6d5be79819db67f0cd6a4f2b4b
Konstantin Chibasov <koheatel@gmail.com>
2020-03-29 09:59:23 +0700

    test: Add unhandled exception middleware tests

---
+++ Vostok.Applications.AspNetCore.Tests/Controllers/ExceptionController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vostok.Applications.AspNetCore.Tests.Controllers
+{
+    [ApiController]
+    [Route("exception")]
+    public class ExceptionController : ControllerBase
+    {
+        [HttpGet]
+        public void Throw() => throw new NotImplementedException();
+    }
+}
---
+++ Vostok.Applications.AspNetCore.Tests/Tests/UnhandledExceptionMiddlewareTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Applications.AspNetCore.Builders;
+using Vostok.Applications.AspNetCore.Tests.Extensions;
+
+namespace Vostok.Applications.AspNetCore.Tests.Tests
+{
+    [TestFixture]
+    public class UnhandledExceptionMiddlewareTests : ControllerTestBase
+    {
+        private const int ResponseCode = 418;
+
+        [Test]
+        public async Task Invoke_ShouldCatch_UnhandledExceptions()
+        {
+            var result = await Client.GetAsync("exception");
+
+            result.Response.Code.Should().Be(ResponseCode);
+        }
+
+        protected override void SetupGlobal(IVostokAspNetCoreApplicationBuilder builder)
+        {
+            builder.SetupUnhandledExceptions(s => s.ErrorResponseCode = ResponseCode);
+        }
+    }
+}
