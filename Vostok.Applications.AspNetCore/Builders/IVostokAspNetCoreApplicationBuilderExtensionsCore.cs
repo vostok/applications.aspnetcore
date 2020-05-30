b9fe8ada6f2e617886557f42186f7d1ddaf658ce
iloktionov <iloktionov@skbkontur.ru>
2020-05-30 19:46:06 +0500

    IVostokApplication as IHostedService.

--- Vostok.Applications.AspNetCore/Builders/IVostokAspNetCoreApplicationBuilderExtensionsCore.cs
+++ Vostok.Applications.AspNetCore/Builders/IVostokAspNetCoreApplicationBuilderExtensionsCore.cs
@@ -2,6 +2,8 @@
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Vostok.Applications.AspNetCore.Helpers;
+using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Applications.AspNetCore.Builders
 {
@@ -11,6 +13,22 @@ namespace Vostok.Applications.AspNetCore.Builders
         public static IVostokAspNetCoreApplicationBuilder AddHostedService<THostedService>([NotNull] this IVostokAspNetCoreApplicationBuilder builder)
             where THostedService : class, IHostedService
             => builder.SetupGenericHost(b => b.ConfigureServices(services => services.AddHostedService<THostedService>()));
+
+        public static IVostokAspNetCoreApplicationBuilder AddHostedServiceFromApplication<TApplication>([NotNull] this IVostokAspNetCoreApplicationBuilder builder)
+            where TApplication : class, IVostokApplication
+            => builder.SetupGenericHost(b => b.ConfigureServices(services =>
+            {
+                services.AddSingleton<TApplication>();
+                services.AddHostedService<VostokHostedService<TApplication>>();
+            }));
+
+        public static IVostokAspNetCoreApplicationBuilder AddHostedServiceFromApplication<TApplication>([NotNull] this IVostokAspNetCoreApplicationBuilder builder, [NotNull] TApplication application)
+            where TApplication : class, IVostokApplication
+            => builder.SetupGenericHost(b => b.ConfigureServices(services =>
+            {
+                services.AddSingleton(application);
+                services.AddHostedService<VostokHostedService<TApplication>>();
+            }));
     }
 }
 #endif
--- Vostok.Applications.AspNetCore/Builders/IVostokNetCoreApplicationBuilderExtensions.cs
+++ Vostok.Applications.AspNetCore/Builders/IVostokNetCoreApplicationBuilderExtensions.cs
@@ -2,6 +2,8 @@
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Vostok.Applications.AspNetCore.Helpers;
+using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Applications.AspNetCore.Builders
 {
@@ -11,6 +13,22 @@ namespace Vostok.Applications.AspNetCore.Builders
         public static IVostokNetCoreApplicationBuilder AddHostedService<THostedService>([NotNull] this IVostokNetCoreApplicationBuilder builder)
             where THostedService : class, IHostedService
             => builder.SetupGenericHost(b => b.ConfigureServices(services => services.AddHostedService<THostedService>()));
+
+        public static IVostokNetCoreApplicationBuilder AddHostedServiceFromApplication<TApplication>([NotNull] this IVostokNetCoreApplicationBuilder builder)
+            where TApplication : class, IVostokApplication
+            => builder.SetupGenericHost(b => b.ConfigureServices(services =>
+            {
+                services.AddSingleton<TApplication>();
+                services.AddHostedService<VostokHostedService<TApplication>>();
+            }));
+
+        public static IVostokNetCoreApplicationBuilder AddHostedServiceFromApplication<TApplication>([NotNull] this IVostokNetCoreApplicationBuilder builder, [NotNull] TApplication application)
+            where TApplication : class, IVostokApplication
+            => builder.SetupGenericHost(b => b.ConfigureServices(services =>
+            {
+                services.AddSingleton(application);
+                services.AddHostedService<VostokHostedService<TApplication>>();
+            }));
     }
 }
 #endif
