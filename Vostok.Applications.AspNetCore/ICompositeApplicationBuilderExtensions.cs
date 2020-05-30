644b0bb23ae7e610fde92a7bd6427d3e703c27cc
iloktionov <iloktionov@skbkontur.ru>
2020-05-30 19:28:22 +0500

    AddNetCore and AddAspNetCore extensions for ICompositeApplicationBuilder.

---
+++ Vostok.Applications.AspNetCore/ICompositeApplicationBuilderExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Vostok.Applications.AspNetCore.Builders;
+using Vostok.Applications.AspNetCore.Models;
+using Vostok.Hosting.Abstractions;
+using Vostok.Hosting.Abstractions.Composite;
+
+namespace Vostok.Applications.AspNetCore
+{
+    [PublicAPI]
+    public static class ICompositeApplicationBuilderExtensions
+    {
+        [NotNull]
+        public static ICompositeApplicationBuilder AddAspNetCore<TStartup>(
+            [NotNull] this ICompositeApplicationBuilder builder,
+            [NotNull] Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup)
+            where TStartup : class => builder.AddApplication(new AdHocAspnetcoreApplication<TStartup>(setup, null));
+
+        [NotNull]
+        public static ICompositeApplicationBuilder AddAspNetCore<TStartup>(
+            [NotNull] this ICompositeApplicationBuilder builder,
+            [NotNull] Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup,
+            [NotNull] Func<IVostokHostingEnvironment, IServiceProvider, Task> warmup)
+            where TStartup : class => builder.AddApplication(new AdHocAspnetcoreApplication<TStartup>(setup, warmup));
+
+        [NotNull]
+        public static ICompositeApplicationBuilder AddAspNetCore(
+            [NotNull] this ICompositeApplicationBuilder builder,
+            [NotNull] Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup)
+            => builder.AddApplication(new AdHocAspnetcoreApplication(setup, null));
+
+        [NotNull]
+        public static ICompositeApplicationBuilder AddAspNetCore(
+            [NotNull] this ICompositeApplicationBuilder builder,
+            [NotNull] Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup,
+            [NotNull] Func<IVostokHostingEnvironment, IServiceProvider, Task> warmup)
+            => builder.AddApplication(new AdHocAspnetcoreApplication(setup, warmup));
+
+        private class AdHocAspnetcoreApplication<TStartup> : VostokAspNetCoreApplication<TStartup>
+            where TStartup : class
+        {
+            private readonly Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup;
+            private readonly Func<IVostokHostingEnvironment, IServiceProvider, Task> warmup;
+
+            public AdHocAspnetcoreApplication(
+                [NotNull] Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup, 
+                [CanBeNull] Func<IVostokHostingEnvironment, IServiceProvider, Task> warmup)
+            {
+                this.setup = setup ?? throw new ArgumentNullException(nameof(setup));
+                this.warmup = warmup ?? ((_, __) => Task.CompletedTask);
+            }
+
+            public override void Setup(IVostokAspNetCoreApplicationBuilder builder, IVostokHostingEnvironment environment)
+                => setup(builder, environment);
+
+            public override Task WarmupAsync(IVostokHostingEnvironment environment, IServiceProvider serviceProvider)
+                => warmup(environment, serviceProvider);
+        }
+
+        private class AdHocAspnetcoreApplication : AdHocAspnetcoreApplication<EmptyStartup>
+        {
+            public AdHocAspnetcoreApplication(
+                [NotNull] Action<IVostokAspNetCoreApplicationBuilder, IVostokHostingEnvironment> setup, 
+                [CanBeNull] Func<IVostokHostingEnvironment, IServiceProvider, Task> warmup)
+                : base(setup, warmup)
+            {
+            }
+        }
+    }
+}
