a8ccd7f73982bf0b9b3bf29fbf3cce2b8c8ed4d6
iloktionov <iloktionov@skbkontur.ru>
2020-06-08 23:00:59 +0500

    Fixed wrong annotations in VostokHealthCheckService.

--- Vostok.Applications.AspNetCore/Diagnostics/VostokHealthCheckService.cs
+++ Vostok.Applications.AspNetCore/Diagnostics/VostokHealthCheckService.cs
@@ -1,9 +1,9 @@
 ﻿#if NETCOREAPP3_1
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
