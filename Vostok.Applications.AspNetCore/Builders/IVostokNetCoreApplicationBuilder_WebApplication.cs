f78e3c79788bc1a26925bfcc467cc82913cce15a
kungurtsev <kungurtsev@skbkontur.ru>
2022-01-10 19:48:56 +0500

    split IVostokNetCoreApplicationBuilder

--- Vostok.Applications.AspNetCore/Builders/IVostokNetCoreApplicationBuilder_WebApplication.cs
+++ Vostok.Applications.AspNetCore/Builders/IVostokNetCoreApplicationBuilder_WebApplication.cs
@@ -3,13 +3,20 @@
 using System;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Vostok.Logging.Microsoft;
 
 namespace Vostok.Applications.AspNetCore.Builders
 {
     [PublicAPI]
     public partial interface IVostokNetCoreApplicationBuilder
     {
+        [Obsolete("Use `IVostokAspNetCoreApplicationBuilder.SetupWebApplicationBuilder(webApplicationBuilder => webApplicationBuilder.Host...)` instead.")]
+        IVostokNetCoreApplicationBuilder SetupGenericHost([NotNull] Action<IHostBuilder> setup);
+
         IVostokNetCoreApplicationBuilder SetupWebApplicationBuilder([NotNull] Action<WebApplicationBuilder> setup);
+        
+        IVostokNetCoreApplicationBuilder SetupMicrosoftLog([NotNull] Action<VostokLoggerProviderSettings> setup);
     }
 }
 
