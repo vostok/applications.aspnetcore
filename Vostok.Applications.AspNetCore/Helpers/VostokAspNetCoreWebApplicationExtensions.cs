6cbb4ef55499878594a4053d30ac624c954cdf02
Anna Teneneva <nddker@gmail.com>
2022-10-19 12:28:57 +0400

    Add initialization check for PingApiMiddleware

--- Vostok.Applications.AspNetCore/Helpers/VostokAspNetCoreWebApplicationExtensions.cs
+++ Vostok.Applications.AspNetCore/Helpers/VostokAspNetCoreWebApplicationExtensions.cs
@@ -1,11 +1,15 @@
 #if NET6_0_OR_GREATER
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Vostok.Applications.AspNetCore.Builders;
 using Vostok.Applications.AspNetCore.Configuration;
+using Vostok.Commons.Threading;
 using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Applications.AspNetCore.Helpers
@@ -20,9 +24,12 @@ namespace Vostok.Applications.AspNetCore.Helpers
             out List<IDisposable> disposables
         )
         {
+            var initializationCheckService = new InitializationCheckLifeTimeService();
+            webApplicationBuilder.Services.AddHostedService(_ => initializationCheckService);
+
             disposables = new List<IDisposable>();
             var vostokBuilder = new VostokAspNetCoreCustomizeWebApplicationBuilder(environment, new EmptyApplication(), disposables);
-            vostokBuilder.SetupPingApi(PingApiSettingsSetup.Get(environment, typeof(WebApplicationBuilder), false));
+            vostokBuilder.SetupPingApi(PingApiSettingsSetup.Get(environment, typeof(WebApplicationBuilder), initializationCheckService.Initialized));
 
             setup(vostokBuilder, environment);
 
@@ -33,6 +40,20 @@ namespace Vostok.Applications.AspNetCore.Helpers
     internal class EmptyApplication : VostokAspNetCoreWebApplication
     {
         public override Task SetupAsync(IVostokAspNetCoreWebApplicationBuilder builder, IVostokHostingEnvironment environment) =>
+            Task.CompletedTask;
+    }
+
+    internal class InitializationCheckLifeTimeService : IHostedService
+    {
+        public AtomicBoolean Initialized { get; } = new AtomicBoolean(false);
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Initialized.TrySet(true);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) =>
             Task.CompletedTask;
     }
 }
