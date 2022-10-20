bf289ee363349fa911d7dc497357bbc91f3228cb
kungurtsev <kungurtsev@skbkontur.ru>
2022-10-20 16:59:45 +0400

    add vostok components

--- Vostok.Applications.AspNetCore/Helpers/ServiceCollectionExtensions.cs
+++ Vostok.Applications.AspNetCore/Helpers/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Vostok.Applications.AspNetCore.Models;
 using Vostok.Configuration.Abstractions;
@@ -11,30 +12,28 @@ using Vostok.Hosting.Abstractions.Requirements;
 
 namespace Vostok.Applications.AspNetCore.Helpers
 {
+    [PublicAPI]
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddVostokEnvironment(this IServiceCollection services, IVostokHostingEnvironment environment, IVostokApplication application)
         {
-            services
-                .AddSingleton(environment)
-                .AddSingleton(environment.ApplicationIdentity)
-                .AddSingleton(environment.ApplicationLimits)
-                .AddTransient(_ => environment.ApplicationReplicationInfo)
-                .AddSingleton(environment.Metrics)
-                .AddSingleton(environment.Log)
-                .AddSingleton(environment.Tracer)
-                .AddSingleton(environment.HerculesSink)
-                .AddSingleton(environment.ConfigurationSource)
-                .AddSingleton(environment.ConfigurationProvider)
-                .AddSingleton(environment.ClusterConfigClient)
-                .AddSingleton(environment.ServiceBeacon)
-                .AddSingleton(environment.ServiceLocator)
-                .AddSingleton(environment.ContextGlobals)
-                .AddSingleton(environment.ContextProperties)
-                .AddSingleton(environment.ContextConfiguration)
-                .AddSingleton(environment.Datacenters)
-                .AddSingleton(environment.HostExtensions);
+            services.AddSingleton(environment);
 
+            services.AddVostokEnvironmentComponents();
+
+            AddVostokEnvironmentHostExtensions(services, environment);
+
+            AddSettingsProviders(services, RequirementDetector.GetRequiredConfigurations(application).Select(r => r.Type), environment.ConfigurationProvider);
+            AddSettingsProviders(services, RequirementDetector.GetRequiredSecretConfigurations(application).Select(r => r.Type), environment.SecretConfigurationProvider);
+            AddSettingsProviders(services, RequirementDetector.GetRequiredMergedConfigurations(application).Select(r => r.Type), environment.ConfigurationProvider);
+
+            services.AddScoped(_ => FlowingContext.Globals.Get<IRequestInfo>());
+
+            return services;
+        }
+
+        public static void AddVostokEnvironmentHostExtensions(this IServiceCollection services, IVostokHostingEnvironment environment)
+        {
             foreach (var (type, obj) in environment.HostExtensions.GetAll())
                 services.AddSingleton(type, obj);
 
@@ -44,12 +43,29 @@ namespace Vostok.Applications.AspNetCore.Helpers
                     .AddSingleton(diagnostics.Info)
                     .AddSingleton(diagnostics.HealthTracker);
             }
+        }
 
-            AddSettingsProviders(services, RequirementDetector.GetRequiredConfigurations(application).Select(r => r.Type), environment.ConfigurationProvider);
-            AddSettingsProviders(services, RequirementDetector.GetRequiredSecretConfigurations(application).Select(r => r.Type), environment.SecretConfigurationProvider);
-            AddSettingsProviders(services, RequirementDetector.GetRequiredMergedConfigurations(application).Select(r => r.Type), environment.ConfigurationProvider);
-
-            services.AddScoped(_ => FlowingContext.Globals.Get<IRequestInfo>());
+        public static IServiceCollection AddVostokEnvironmentComponents(this IServiceCollection services)
+        {
+            services
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ApplicationIdentity)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ApplicationIdentity)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ApplicationLimits)
+                .AddTransient(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ApplicationReplicationInfo)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().Metrics)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().Log)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().Tracer)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().HerculesSink)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ConfigurationSource)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ConfigurationProvider)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ClusterConfigClient)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ServiceBeacon)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ServiceLocator)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ContextGlobals)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ContextProperties)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().ContextConfiguration)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().Datacenters)
+                .AddSingleton(provider => provider.GetRequiredService<IVostokHostingEnvironment>().HostExtensions);
 
             return services;
         }
