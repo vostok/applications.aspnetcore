3a9a612fd82a7b33ec0763e6f6641635dcb9d9b9
koheatel <koheatel@gmail.com>
2022-12-16 14:46:10 +0600

    Temporary add InternalsVisibleTo attribute for hosting.aspnetcore module

--- Vostok.Applications.AspNetCore/Properties/AssemblyInfo.cs
+++ Vostok.Applications.AspNetCore/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Vostok.Applications.AspNetCore.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("Vostok.Hosting.AspNetCore")]
