36da8d1833a1af70260435eb2c1d1006285c474c
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-22 17:49:45 +0400

    rename VostokHostedService to VostokApplicationBackgroundService

--- Vostok.Applications.AspNetCore/Helpers/VostokApplicationBackgroundService.cs
+++ Vostok.Applications.AspNetCore/Helpers/VostokApplicationBackgroundService.cs
@@ -6,13 +6,13 @@ using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Applications.AspNetCore.Helpers
 {
-    internal class VostokHostedService<TApplication> : BackgroundService
+    internal class VostokApplicationBackgroundService<TApplication> : BackgroundService
         where TApplication : IVostokApplication
     {
         private readonly TApplication application;
         private readonly IVostokHostingEnvironment environment;
 
-        public VostokHostedService(TApplication application, IVostokHostingEnvironment environment)
+        public VostokApplicationBackgroundService(TApplication application, IVostokHostingEnvironment environment)
         {
             this.application = application;
             this.environment = environment;
