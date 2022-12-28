361ae082b36452490f9f5d8afa5776898de11cae
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-28 17:27:53 +0400

    remove InternalsVisibleTo("Vostok.Hosting.AspNetCore")

--- Vostok.Applications.AspNetCore/Properties/AssemblyInfo.cs
+++ Vostok.Applications.AspNetCore/Properties/AssemblyInfo.cs
@@ -2,4 +2,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Vostok.Applications.AspNetCore.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("Vostok.Hosting.AspNetCore")]
