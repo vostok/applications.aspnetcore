bbcaaecdc925a7163d154a706e86b74ea492cc84
shalin <shalin@skbkontur.ru>
2023-10-06 15:44:16 +0500

    add LogResponseStatusCodes

---
+++ Vostok.Applications.AspNetCore/Configuration/LoggingSettingsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Applications.AspNetCore.Configuration
+{
+    [PublicAPI]
+    internal static class LoggingSettingsExtensions
+    {
+        /// <summary>
+        /// Determines whether to response codes of outbound responses for log.
+        /// </summary>
+        public static void SetLogResponseStatusCodes([NotNull] this LoggingSettings settings, params int[] logResponseStatusCodes) =>
+            settings.LogResponseStatusCodes = logResponseStatusCodes;
+    }
+}
