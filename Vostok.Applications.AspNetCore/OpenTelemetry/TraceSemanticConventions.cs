eb53c65ea6e88166b40b439c4ac02a5b5ee464d1
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2024-09-05 15:01:46 +0200

    Get rid of dependency

---
+++ Vostok.Applications.AspNetCore/OpenTelemetry/TraceSemanticConventions.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable once CheckNamespace
+namespace OpenTelemetry.Trace;
+
+// note (ponomaryovigor, 05.09.2024): Temporarily copied from legacy OpenTelemetry.SemanticConventions package
+// because it was deprecated and unlisted
+internal static class TraceSemanticConventions
+{
+    public const string AttributeHttpRequestContentLength = "http.request_content_length";
+    public const string AttributeHttpResponseContentLength = "http.response_content_length";
+    public const string AttributeHttpClientIp = "http.client_ip";
+}
